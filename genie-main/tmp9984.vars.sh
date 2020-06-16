3cf26b32d1baa0dd717029219cd3e5e10531d4c7
Camillalxy98 <liu.camilla@yahoo.com>
2020-06-16 17:35:00 -0500

    Initial commit

---
+++ genie-main/tmp9984.vars.sh
@@ -0,0 +1,10 @@
+OUTROOT=/home/sulphurtuft/cgenie_output
+CODEDIR=/home/sulphurtuft/cgenie.muffin
+RUNTIME_ROOT=../../cgenie.muffin
+RUNTIME_OUTDIR=.
+EXPID=AOMTM.igcm16.RUN
+FIXEDATMOSFILE=data_fixedatmos
+GLIMMERFILE=data_glimmer.config
+igcm_module=genie-igcm
+BFGROOT=EMPTY
+MODULE_NAMES='plasim goldstein goldsteinseaice biogem atchem '
